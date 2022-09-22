d839758a1ea312ff2820b83264a10a3f1310dfcd
Nuttadon <nuttadon_p@cmu.ac.th>
2022-09-22 14:50:19 +0700

    s

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -7,14 +7,14 @@ export default function roomIdMessageRoute(req, res) {
     const id = req.query.roomId;
     const roomIdx = rooms.findIndex((x) => x.roomId === id);
     const rmessages = rooms[roomIdx].messages;
-    if (roomIdx == -1)
-      return res.status(404).json({ ok: false, message: "Invalid room id" });
-    else {
+    if (roomIdx !== -1) {
       const result = rmessages.map((x) => ({
         messageId: x.messageId,
         text: x.text,
       }));
       return res.json({ ok: true, messages: result });
+    } else {
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
     }
   } else if (req.method === "POST") {
     const rooms = readDB();

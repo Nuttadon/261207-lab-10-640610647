63c00e0daaa80a899aaac531cdbcd87c428fa48b
Nuttadon <nuttadon_p@cmu.ac.th>
2022-09-22 15:25:00 +0700

    d

--- pages/api/me.js
+++ pages/api/me.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  return res.json({ name: "Nuttadon Punyapan", studentId: "64061064" });
+  return res.json({ name: "Nuttadon Punyapan", studentId: "640610647" });
 }
--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -11,12 +11,25 @@ export default function roomIdMessageRoute(req, res) {
     else return res.json({ ok: true, messages: rooms[roomIdx].messages });
   } else if (req.method === "POST") {
     const rooms = readDB();
-
     //read request body
     const text = req.body.text;
-
     //create new id
     const newId = uuidv4();
+    const id = req.query.roomId;
+    const roomIdx = rooms.findIndex((x) => x.roomId === id);
+    if (roomIdx === -1)
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    else if (typeof req.body.text !== "string")
+      return res.status(404).json({ ok: false, message: "Invalid text input" });
+    else {
+      const newmess = {
+        messageId: newId,
+        text: req.body.text,
+      };
+      rooms[roomIdx].messages.push(newmess);
+      writeDB(rooms);
+      return res.json({ ok: true, message: newmess });
+    }
   }
 }
 

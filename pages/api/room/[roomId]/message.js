d3ea5ad7e3d7e2cf59d234619d68ed5c5bf7c7b9
Nuttadon <nuttadon_p@cmu.ac.th>
2022-09-22 14:42:03 +0700

    s

--- pages/api/room/[roomId]/message.js
+++ pages/api/room/[roomId]/message.js
@@ -4,6 +4,18 @@ import { v4 as uuidv4 } from "uuid";
 export default function roomIdMessageRoute(req, res) {
   if (req.method === "GET") {
     const rooms = readDB();
+    const id = req.query.roomId;
+    const roomIdx = rooms.findIndex((x) => x.roomId === id);
+    const rmessages = rooms[roomIdx].messages;
+    if (roomIdx === -1)
+      return res.status(404).json({ ok: false, message: "Invalid room id" });
+    else {
+      const result = rmessages.map((x) => ({
+        messageId: x.messageId,
+        text: x.text,
+      }));
+      return res.json({ ok: true, messages: result });
+    }
   } else if (req.method === "POST") {
     const rooms = readDB();
 
@@ -14,3 +26,42 @@ export default function roomIdMessageRoute(req, res) {
     const newId = uuidv4();
   }
 }
+
+// export default function todoRoute(req, res) {
+//   if (req.method === "GET") {
+//     const todolist = readDB();
+//     return res.json({ ok: true, todolist: todolist });
+//   } else if (req.method === "POST") {
+//     const todolist = readDB();
+//     const newTodo = {
+//       id: uuidv4(),
+//       title: req.body.title,
+//       completed: req.body.completed,
+//     };
+//     todolist.push(newTodo);
+//     writeDB(todolist);
+//     return res.json({ ok: true });
+//   }
+
+//   if (req.method === "DELETE") {
+//     const todolist = readDB();
+//     const id = req.query.id;
+//     const todoIdx = todolist.findIndex((x) => x.id === id);
+//     if (todoIdx === -1)
+//       return res.status(404).json({ ok: false, message: "Todo is not found" });
+
+//     todolist.splice(todoIdx, 1);
+//     writeDB(todolist);
+//     return res.json({ ok: true });
+//   } else if (req.method == "PUT") {
+//     const todolist = readDB();
+//     const id = req.query.id;
+//     const todoIdx = todolist.findIndex((x) => x.id === id);
+//     if (todoIdx === -1)
+//       return res.status(404).json({ ok: false, message: "Todo is not found" });
+//     if (todolist[todoIdx].completed === true)
+//       todolist[todoIdx].completed = false;
+//     else todolist[todoIdx].completed = true;
+//     writeDB(todolist);
+//     return res.json({ ok: true, todo: todolist[todoIdx] });
+//   }
